90c113012a2bf72d403168bf5c6d26aa1ff2c2cd
Abdirahman <abdirahmandubow4@gmail.com>
2024-12-16 21:13:00 +0800

    updating lecture.py file

--- Computation/lecture1.py
+++ Computation/lecture1.py
@@ -1,3 +1,16 @@
 #getting random number
 import random
 print(random.randint(1,1000))
+
+# looking for the type of Scalar Objects
+n = 5
+print(type(n)) # you should get <class 'int'>
+
+x= 7.2
+print(type(x)) # you should get <class 'float'>
+
+a = True
+print(type(a)) # you should get <class 'bool'>
+
+b = None
+print(type(b)) # you should get <class 'NoneType'>
