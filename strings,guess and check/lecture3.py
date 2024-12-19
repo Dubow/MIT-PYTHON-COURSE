9371914e091cb3748c7e33481405d50bcd76270f
Abdirahman <abdirahmandubow4@gmail.com>
2024-12-19 18:39:55 +0800

    updating lecture3.py

--- strings,guess and check/lecture3.py
+++ strings,guess and check/lecture3.py
@@ -1,1 +1,4 @@
 #  String Manipulation, Guess and Check, Approximations, Bisection
+s = 'abc' #string
+print(len(s)) # getting the length of string s
+print(s[0]) # getting the position since the index in python starts from 0 and -1 from the last
