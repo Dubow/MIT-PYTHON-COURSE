0b7497e5b28e415cb252aaf6f694bf561ab81c23
Abdirahman <abdirahmandubow4@gmail.com>
2024-12-20 22:10:46 +0800

    updating lecture4.py

--- Functions/lecture4.py
+++ Functions/lecture4.py
@@ -1,1 +1,10 @@
 # Lecture 4: Decomposition, Abstraction, and Functions
+# look for whether the number is even or not
+def is_even(number):
+    if number % 2 == 0:
+        return('even number')
+    else:
+        return('odd number')
+number = int(input("Input your number please: "))
+result = is_even(number)
+print(f"The number {number} is an {result}")
