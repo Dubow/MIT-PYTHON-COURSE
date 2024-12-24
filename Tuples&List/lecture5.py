54c43e52ef8b3cbae7ba40151ac688708fd1686d
Abdirahman <abdirahmandubow4@gmail.com>
2024-12-24 22:06:01 +0800

    updating lecture5.py

--- Tuples&List/lecture5.py
+++ Tuples&List/lecture5.py
@@ -1,4 +1,14 @@
 # Tuples= They are immutable(once created cannot be changed)
 # Tuples are created using parenthesis()
 t = (2, "sut", 10)
-print(t[0])
+print(t[0]) #prints out 2
+x = (2, "mit", 10)+(5,6)
+print(x) #prints out the added list
+
+def quotient_and_remainder(x,y):
+    q = x//y
+    r = x%y
+    return q,r
+(x,y) = (4,3)
+result = quotient_and_remainder(x,y)
+print(result)
